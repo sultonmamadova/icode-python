7c668d036676378d8775de25d3c0f5a655bfb192
shocodeart <shocodeart@gmail.com>
2022-09-03 15:50:53 +0500

    day 1

---
+++ day1/hello.py
@@ -0,0 +1,86 @@
+"""This module prints hello world."""
+
+# v1 
+print("Hello, American Space")
+print()
+
+# v2
+print("Creating and using of variables")
+message = "Hello, American Space"
+print(message)
+print()
+
+# Math
+print("Math operations")
+print(1 + 2) # plus
+print(1 * 2) # times
+print(1 / 2) # devision
+print()
+
+# Combine  
+a = 10
+b = 10
+print("a / b =", a / b)
+print()
+
+# Float 
+1.0 
+1.234234234
+
+# int
+1233
+123314
+234234
+
+# str
+"Hello"
+'python'
+"don't"
+
+# comment 
+
+messsage_hello = "hello, space"
+
+# don't re-write builtins
+# int = 12354 # DON'T DO THAT
+int_ = "123124" # BETTER
+
+
+# Variables creation cases
+# PascalCase
+# camelCase
+# snake_case
+# kebab-case
+
+def too_important_function(): # snake case
+    pass
+
+class SomeCoolClass: # pascal case
+    pass
+
+a = 1 
+b = 2 
+
+# name = str(input("Enter your name: ")) 
+# print(name)
+
+# name
+# surname
+# age
+# phone
+
+# type
+print(type("some string"))
+print(type(123))
+print(type(123.123))
+print()
+
+some_variable = "12313"
+print("typoeof some_variable is", type(some_variable))
+print()
+
+# exception (error)
+# some_variable + 1234
+
+
+print("string ", 1)
