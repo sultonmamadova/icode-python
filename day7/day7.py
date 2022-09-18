0ec5f4e93b89b9073b435f7d601f919afd95e6e0
shocodeart <shocodeart@gmail.com>
2022-09-18 12:49:44 +0500

    day 7

---
+++ day7/day7.py
@@ -0,0 +1,95 @@
+
+# persons = {
+#         "first_name": "Shodmon",
+#         "last_name": "Ravshanov",
+#         "age": 27
+#     }
+
+ 
+# db = {
+#     1: {
+#         "first_name": "Shodmon",
+#     },
+#     2: {
+#         "first_name": "Karina",
+#     },
+#     3: {
+#         "first_name": "Anisa",
+#     }
+# }
+
+# print(db[1]['first_name'])
+
+# db2 = db[1] # {
+#             #     "first_name": "Shodmon",
+#             # }
+# print(db2)
+# print(type(db2)) # dict
+
+
+# Создайте словарь person с ключами 
+# 1  здесь пускай будет возраст
+# first_name любое имя
+# city любой город
+
+
+# db = {
+#     0: "Manizha",
+#     1: "Shodmon",
+#     32423432: "Shodmon",
+#     2: "Karina",
+#     3: "Anisa",
+# }
+
+# print(db)
+# print(db[1])
+
+# name = db[1] # "Shodmon"
+# print(name)
+
+# name = db[1] # "Shodmon"
+# print(name)
+
+# name = db["1"] 
+# print(name)
+
+db = []
+db = {}
+
+db["last_name"] = "shodmon"
+db["last_name"] = "shodmon12313214"
+db["last_name"] = "shodmon121312341232313214"
+db["last_name"] = "karina"
+
+# {"last_name": "shodmon"}
+
+# print(db["last_name"])
+print(db)
+
+
+
+# persons = {
+#     "shodmon@email.com": {
+#         "first_name": "Shodmon",
+#         "last_name": "Ravshanov",
+#         "age": 27
+#     },
+#     "zarina@email.com": {
+#         "first_name": "Zarina",
+#         "last_name": "Kadamova",
+#         "age": 18
+#     },
+#     "marjona@email.com": {
+#         "first_name": "Marjona",
+#         "last_name": "Khusnieva",
+#         "age": 33
+#     },
+#     "nigina@email.com": {
+#         "first_name": "Shodmon",
+#         "last_name": "Ravshanov",
+#         "age": 27
+#     }
+# }
+
+
+# print(persons["marjona@email.com"]["first_name"])
