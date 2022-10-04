c6e00be065113735cd4c25345f31119a87005c47
shocodeart <shocodeart@gmail.com>
2022-10-04 22:32:14 +0500

    day 13

--- day13/main.py
+++ day13/main.py
@@ -7,8 +7,9 @@ from db import db as db_data
 from forms.person_form import PersonForm
 
 
-print(db)
-print(db_data)
+if __name__ == "__main__":
+    print(db)
+    print(db_data)
 
-form = PersonForm(name="Shodmon", surname="Ravshanov", age=19)
-print(form.to_dict())
+    form = PersonForm(name="Shodmon", surname="Ravshanov", age=19)
+    print(form.to_dict())
